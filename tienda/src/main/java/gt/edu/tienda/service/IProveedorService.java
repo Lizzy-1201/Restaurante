3f422acab85baba1c54433c7230092c6bea0d363
victorFloresDubon <vdubon@outlook.com>
2020-10-25 17:56:10 -0600

    Arreglé el getByLikeNombre de Proveedor para que reconozca las minúsculas

--- tienda/src/main/java/gt/edu/tienda/service/IProveedorService.java
+++ tienda/src/main/java/gt/edu/tienda/service/IProveedorService.java
@@ -13,7 +13,7 @@ public interface IProveedorService extends GenericServiceAPI<Proveedor, Integer>
 	
 	Optional<Proveedor> findByNombre(String nombre);
 
-	@Query(value = "SELECT * FROM proveedor p WHERE p.nombre LIKE '%'||UPPER(:nombre)||'%'", 
+	@Query(value = "SELECT * FROM proveedor p WHERE UPPER(p.nombre) LIKE '%'||UPPER(:nombre)||'%'", 
 			nativeQuery = true)
 	List<Proveedor> findByLikeNombre(@Param("nombre")String nombre);
 
