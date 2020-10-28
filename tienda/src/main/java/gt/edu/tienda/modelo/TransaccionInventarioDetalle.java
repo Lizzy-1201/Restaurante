6b9d255a7826bf2dbeb7e1b7ccefdc2848ef3ca2
victorFloresDubon <vdubon@outlook.com>
2020-10-28 11:26:06 -0600

    Arreglé los métodos de búsqueda por fecha en el repositorio y servicio de TransaccionInventario

--- tienda/src/main/java/gt/edu/tienda/modelo/TransaccionInventarioDetalle.java
+++ tienda/src/main/java/gt/edu/tienda/modelo/TransaccionInventarioDetalle.java
@@ -2,7 +2,6 @@ package gt.edu.tienda.modelo;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
